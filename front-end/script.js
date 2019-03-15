f468fb4b6cf82b888353103e4dbc0e21494e5a1e
javascript5 <javascript5@hotmail.com>
2019-03-15 13:55:36 +0700

    [Add] initial file :tada:

---
+++ front-end/script.js
@@ -0,0 +1,23 @@
+$('document').ready(function () {
+    $(".post_filter").on('change', function () {
+        var postFilterValue = $(this).val();
+        $(".post_item").each(function () {
+            var postAttr = $(this).attr("category");
+
+            if (postFilterValue == postAttr) {
+                $(this).css({ "display": "flex" });
+            } else {
+                $(this).css({ "display": "none" });
+            }
+        });
+    });
+
+    $('.list_button').click(function () {
+        $('.post_item').addClass("list_item");
+        $('.post_item').removeClass("grid_item");
+    });
+    $('.grid_button').click(function () {
+        $('.post_item').removeClass("list_item");
+        $('.post_item').addClass("grid_item");
+    });
+});
